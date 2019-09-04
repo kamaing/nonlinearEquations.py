5effd3d28bb665b5dd114b999e65427e53073e84
K4114Ing <37227939+kamaing@users.noreply.github.com>
2019-09-04 04:18:06 +0200

    Update nonlinearEquations.py

--- nonlinearEquations.py
+++ nonlinearEquations.py
@@ -1,15 +1,17 @@
 from gekko import GEKKO
 m = GEKKO()             # create GEKKO model
+m._path = r'C:\test'    # save modell to files
 
 # Constants and Variables
-a = 40 
+a = m.Var(value=0.5) 
 x = m.Var(value=0)      # define new variable, initial value=0
 y = m.Var(value=1)      # define new variable, initial value=1
 
 # Equations
 m.Equations([x + 2*y==0,     # equations
-             x**2+y**2==1]) 
+             x**2+y**2==1,
+             a+x==1]) 
 
 # solve
 m.solve(disp=False)     
-print([x.value[0],y.value[0]]) # print solution
+print([x.value[0],y.value[0],a.value[0]]) # print solution
